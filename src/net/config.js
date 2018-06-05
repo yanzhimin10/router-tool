23f036b95c95f91a8b10f9060714f06fd889b38d
yzm <zm_yan@droidhang.com>
2018-06-05 15:35:21 +0800

    server_url修改

--- src/net/config.js
+++ src/net/config.js
@@ -1,7 +1,7 @@
 var server_url = "https://agl.dhgames.cn:8088/";
 
 if (process.env.NODE_ENV == "development") {
-    server_url = "http://192.168.102.226:8088/"; //company
+    server_url = "http://192.168.102.226:8087/"; //company
     // server_url = "http://192.168.1.10:8088/"; //home
 }
 
